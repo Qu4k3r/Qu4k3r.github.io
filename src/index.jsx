891a4739b4de7058d9d62f04b389a8aab31f34ce
qu4k3r <neves0007@gmail.com>
2021-10-07 23:26:59 -0300

    Adiciona tagManager em index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
+import TagManager from 'react-gtm-module';
+
 import './index.css';
 import App from './App';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-57N79LK',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
   <Router>
