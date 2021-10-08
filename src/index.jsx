87fd99da3a64059789f8168bdd95413a25bd24a3
qu4k3r <neves0007@gmail.com>
2021-10-08 13:45:24 -0300

    Adiciona react-ga4

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 import TagManager from 'react-gtm-module';
+import ReactGA from 'react-ga4';
 
 import './index.css';
 import App from './App';
+
+ReactGA.initialize('G-VXNLE0WQGC');
+ReactGA.send({ hitType: 'pageview', page: '/#' });
 
 const tagManagerArgs = {
   gtmId: 'GTM-57N79LK',
